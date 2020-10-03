c46e37fc94b1e677d154177f71e9ddd52ba87522
eitamar <eitamarsaraf@gmail.com>
2020-10-04 00:08:50 +0300

    add Consts class import

--- data_handler/loaders.py
+++ data_handler/loaders.py
@@ -1,18 +1,18 @@
 from torch.utils.data import DataLoader
 from data_handler.gcommand_loader import GCommandLoader
+from const import Consts
 
 
 def get_data_loaders():
     # load the data
-    batch_size = 32
     train_dataset = GCommandLoader('dataset/train')
     train_loader = DataLoader(
-        train_dataset, batch_size=batch_size, shuffle=True)
+        train_dataset, batch_size=Consts.batch_size, shuffle=True)
 
     valid_dataset = GCommandLoader('dataset/valid')
     valid_loader = DataLoader(
-        valid_dataset, batch_size=batch_size)
+        valid_dataset, batch_size=Consts.batch_size, shuffle=True)
     test_dataset = GCommandLoader('dataset/test', is_test=True)
     test_loader = DataLoader(
-        test_dataset, batch_size=batch_size)
+        test_dataset, batch_size=Consts.batch_size, shuffle=True)
     return train_loader, valid_loader, test_loader
