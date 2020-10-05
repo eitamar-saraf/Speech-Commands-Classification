1388f2f82b79adbfafa83941ae9cb87002be5645
eitamar <eitamarsaraf@gmail.com>
2020-10-05 14:19:02 +0300

    add dropout layer

--- model.py
+++ model.py
@@ -11,6 +11,7 @@ class LeNet(nn.Module):
         self.batch_norm2 = nn.BatchNorm2d(64)
         self.max_pool2 = nn.MaxPool2d(kernel_size=2)  # (64, 38, 23)
         self.fc1 = nn.Linear(55936, 1000)
+        self.dropout = nn.Dropout(0.25)
         self.fc2 = nn.Linear(1000, output_size)
         self.activation = nn.ReLU()
 
@@ -23,7 +24,7 @@ class LeNet(nn.Module):
         x = self.max_pool2(x)
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
-        x = self.activation(x)
+        x = self.dropout(self.activation(x))
         x = self.fc2(x)
         x = nn.functional.log_softmax(x, dim=-1)
         return x
