38ff9b427ce543141a01c6dc4e2961d4f95136aa
eitamar <eitamarsaraf@gmail.com>
2023-01-15 23:53:21 +0200

    fix dataset split

--- data_handling/create_dataset.py
+++ data_handling/create_dataset.py
@@ -1,47 +1,53 @@
 import os
 import shutil
+from pathlib import Path
+import logging
+
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
 
 
-def move_files(original_fold, data_fold, data_filename):
-    with open(data_filename) as f:
-        for line in f.readlines():
-            vals = line.split('/')
-            dest_fold = os.path.join(data_fold, vals[0])
-            if not os.path.exists(dest_fold):
-                os.mkdir(dest_fold)
-            shutil.move(os.path.join(original_fold, line[:-1]), os.path.join(data_fold, line[:-1]))
+def move_files(original_fold: Path, data_fold: Path, data_filename_list: Path) -> None:
+    with open(data_filename_list) as f:
+        for filename in tqdm(f.readlines()):
+            splitted_filename = filename.strip().split('/')
+            destination_fold = data_fold.joinpath(splitted_filename[0])
+            if not destination_fold.exists():
+                os.mkdir(destination_fold)
+            shutil.move(original_fold.joinpath(filename.strip()), data_fold.joinpath(filename.strip()))
 
 
-def create_train_fold(original_fold, data_fold, test_fold):
-    # list dirs
+def create_train_fold(original_fold: Path, data_fold: Path, test_fold: Path) -> None:
+    # Take only samples
     dir_names = list()
-    for file in os.listdir(test_fold):
-        if os.path.isdir(os.path.join(test_fold, file)):
-            dir_names.append(file)
+    for folder in test_fold.iterdir():
+        if folder.is_dir():
+            dir_names.append(folder.name)
 
     # build train fold
-    for file in os.listdir(original_fold):
-        if os.path.isdir(os.path.join(test_fold, file)) and file in dir_names:
-            shutil.move(os.path.join(original_fold, file), os.path.join(data_fold, file))
+    for folder in original_fold.iterdir():
+        if folder.is_dir() and folder.name in dir_names:
+            shutil.move(folder, data_fold.joinpath(folder.name))
 
 
-def make_dataset(gcommands_fold, out_path):
-    validation_path = os.path.join(gcommands_fold, 'validation_list.txt')
-    test_path = os.path.join(gcommands_fold, 'testing_list.txt')
+def make_dataset(speech_commands_folder: Path, out_path: Path) -> None:
+    validation_path = speech_commands_folder.joinpath('validation_list.txt')
+    test_path = speech_commands_folder.joinpath('testing_list.txt')
 
-    valid_fold = os.path.join(out_path, 'valid')
-    test_fold = os.path.join(out_path, 'test')
-    train_fold = os.path.join(out_path, 'train')
+    validation_fold = out_path.joinpath('validation')
+    test_fold = out_path.joinpath('test')
+    train_fold = out_path.joinpath('train')
 
-    if not os.path.exists(out_path):
-        os.mkdir(out_path)
-    if not os.path.exists(valid_fold):
-        os.mkdir(valid_fold)
-    if not os.path.exists(test_fold):
-        os.mkdir(test_fold)
-    if not os.path.exists(train_fold):
-        os.mkdir(train_fold)
+    for path in [validation_fold, test_fold, train_fold]:
+        if not path.exists():
+            path.mkdir(parents=True, exist_ok=True)
 
-    move_files(gcommands_fold, test_fold, test_path)
-    move_files(gcommands_fold, valid_fold, validation_path)
-    create_train_fold(gcommands_fold, train_fold, test_fold)
+    logger.info('Moving test files')
+    move_files(speech_commands_folder, test_fold, test_path)
+
+    logger.info('Moving validation files')
+    move_files(speech_commands_folder, validation_fold, validation_path)
+
+    logger.info('Moving train files')
+    create_train_fold(speech_commands_folder, train_fold, test_fold)
--- main.py
+++ main.py
@@ -1,33 +1,54 @@
 import argparse
+from pathlib import Path
+import sys
 
 from data_handling.create_dataset import make_dataset
 from data_handling.transform_dataset import transform_dataset
 from train import train_lenet, test
 from utils import get_device
 
+import logging
+
+logging.basicConfig(format='%(asctime)s, %(levelname)s:, %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
+                    stream=sys.stdout, level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Parser that controls the script.')
 
-    parser.add_argument('action', help='the action that the script should do.')
-    parser.add_argument('--gcommands_fold',
-                        help='the path to the root folder of te google commands dataset before splitting.')
-    parser.add_argument('--out_path', default='dataset', help='the path where to save the files splitted to folders.')
-    parser.add_argument('--dataset', help='the path to the root folder of te google commands dataset after splitting.')
+    parser.add_argument('--action', help='the action that the script should do.',
+                        choices=['train', 'test', 'create_dataset', 'transform_dataset'], default='create_dataset')
+    parser.add_argument('--speech_commands_folder',
+                        help='the path to the root folder of te google commands dataset before splitting.',
+                        default=r'dataset/raw')
+    parser.add_argument('--out_path', help='the path where to save the files splitted to folders.',
+                        default='dataset/splitted')
+    parser.add_argument('--dataset', help='the path to the root folder of te google commands dataset after splitting.',
+                        default='dataset/splitted')
     parser.add_argument('--test_dataset', help='the path to the root folder of the test dataset.')
     parser.add_argument('--model', help='the path to the model.')
 
     args = parser.parse_args()
 
     if args.action == 'create_dataset':
-        make_dataset(args.gcommands_fold, args.out_path)
+        logger.info('Creating dataset')
+        make_dataset(Path(args.speech_commands_folder), Path(args.out_path))
 
     elif args.action == 'transform_dataset':
+        logger.info('Transforming dataset')
         transform_dataset(args.dataset)
 
     elif args.action == 'train':
+        logger.info('Training model')
         device = get_device()
         train_lenet(device, args.dataset)
 
     elif args.action == 'test':
+        logger.info('Testing model')
         device = get_device()
         test(device, args.test_dataset, args.model)
+
+    else:
+        logger.error('Action not recognized')
+        raise ValueError('Action not supported')
